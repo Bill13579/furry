2213059a3745b995800c77367989e085e62514b6
Bill Kudo <bluesky42624@gmail.com>
2019-05-14 21:26:10 +0900

    Fixed range of randn

--- furry/init/init.py
+++ furry/init/init.py
@@ -16,7 +16,7 @@ def rand(shape, out=None, dtype=furry.data.float32, layout=strided, device=furry
     return 2 * torch.rand(*shape, out=out, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad) - 1
 
 def randn(shape, out=None, dtype=furry.data.float32, layout=strided, device=furry.data.default_device, requires_grad=False):
-    return 2 * torch.randn(*shape, out=out, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad) - 1
+    return torch.randn(*shape, out=out, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad)
 
 def he(shape, dtype=furry.data.float32, layout=strided, device=furry.data.default_device, requires_grad=False):
     return furry.init.heuristic.he(rand(shape, dtype=dtype, layout=layout, device=device, requires_grad=requires_grad))
