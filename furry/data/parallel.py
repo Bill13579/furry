bb8b0f76164cf3baea35f1d57b7534399f654960
Bill Kudo <bluesky42624@gmail.com>
2019-05-20 21:36:20 +0900

    Removed `OnTheFlyData`

--- furry/data/parallel.py
+++ furry/data/parallel.py
@@ -48,37 +48,3 @@ class ParallelLoadedData(Data):
         else:
             batch = super().nbatch(batch_size=batch_size)
         return batch
-
-class OnTheFlyData(Data):
-    def __init__(self):
-        super().__init__([None], [None])
-    
-    def __has_ntei(self):
-        return (self.x[-1] is None) and (self.y[-1] is None) and (self.shuffled_x[-1] is None) and (self.shuffled_y[-1] is None)
-
-    def __remove_ntei_element(self, arr):
-        if arr[-1] is None:
-            del arr[-1]
-
-    def __remove_ntei(self):
-        self.__remove_ntei_element(self.x)
-        self.__remove_ntei_element(self.y)
-        self.__remove_ntei_element(self.shuffled_x)
-        self.__remove_ntei_element(self.shuffled_y)
-
-    def single(self):
-        raise NotImplementedError()
-    
-    def nbatch(self, batch_size=1):
-        if self.__has_ntei():
-            self.__remove_ntei()
-            for i in range(batch_size):
-                dp = self.single()
-                if "x" not in dp or "y" not in dp:
-                    raise ValueError("single() must return a dict with keys x and y")
-                self.append(dp["x"], dp["y"])
-                if "done" in dp and dp["done"]:
-                    self.__remove_ntei()
-                    break
-            self.append(None, None)
-        return super().nbatch(batch_size=batch_size)
