19afba81d53a61641a63179d749dc36adc2cb130
Bill Kudo <bluesky42624@gmail.com>
2019-06-22 19:22:24 +0900

    Release 1.1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setup(name="furry",
-    version="1.1.0",
+    version="1.1.0.2",
     description="",
     long_description=long_description,
     author="Bill Kudo",
@@ -13,6 +13,7 @@ setup(name="furry",
     license="MIT",
     packages=find_packages(),
     scripts=[],
+    install_requires=["torch"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
